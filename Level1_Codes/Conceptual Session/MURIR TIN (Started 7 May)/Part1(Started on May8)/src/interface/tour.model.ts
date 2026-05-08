863c47e1e0b1bcaaf92fd54a82507f15c5a4c432
Rahul Rudra <rahulrudra665@gmail.com>
2026-05-08 19:45:04 +0600

    changess made"

---
+++ Level1_Codes/Conceptual Session/MURIR TIN (Started 7 May)/Part1(Started on May8)/src/interface/tour.model.ts
@@ -0,0 +1,14 @@
+interface ITour{
+    name: string
+    durationHours: number
+    ratingsAverage: number
+    ratingsQuality: number
+    price: number
+    imageCover: string
+    images: string[]
+    createdAt: Date
+    startDates: Date[]
+    locations: string[]
+    startLocation:string
+slug:string
+}
--- Level1_Codes/Conceptual Session/MURIR TIN (Started 7 May)/Part1(Started on May8)/src/server/server.ts
+++ Level1_Codes/Conceptual Session/MURIR TIN (Started 7 May)/Part1(Started on May8)/src/server/server.ts
@@ -1,5 +1,7 @@
+import mongoose from "mongoose"
 import app from "../app/app"
 const port=8000
 app.listen(port, () => {
+  mongoose.connect("mongodb://localhost:27017/MurirTin5sthMay")
   console.log(`Example app listening on port1 ${port}`)
 })
