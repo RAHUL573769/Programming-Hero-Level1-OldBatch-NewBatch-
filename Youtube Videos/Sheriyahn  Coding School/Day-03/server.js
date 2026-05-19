a976a3f45493d0d2755a32a3a77f0fd641621358
Rahul Rudra <rahulrudra665@gmail.com>
2026-05-19 09:35:14 +0600

    changes made"

---
+++ Youtube Videos/Sheriyahn  Coding School/Day-03/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+    console.log("Middle Ware Chala");
+    next(); // move to next middleware/route
+});
+
+app.get("/", (req, res) => {
+    res.send("Champio mra anuj");
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
