7fe82b7301ef5b2768c392e08184b00499bf7e17
Rahul Rudra <rahulrudra665@gmail.com>
2026-05-20 08:30:49 +0600

    changes made"

--- Youtube Videos/Sheriyahn  Coding School/Day-05 - Copy/server.js
+++ Youtube Videos/Sheriyahn  Coding School/Day-05 - Copy/server.js
@@ -1,18 +1,31 @@
-const express = require("express");
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
+// Fix __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use((req, res, next) => {
     console.log("Middle Ware Chala");
-    next(); // move to next middleware/route
+    next();
 });
+
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.send("Champio mra anuj");
+});
+
+app.get("/ejs", (req, res) => {
+    res.render("index");
 });
 
 app.listen(3000, () => {
